1635182bd6aa4871726133f381a707e73ba9dc7d
aslpavel <asl.pavel@gmail.com>
2013-04-01 12:41:42 +0400

    [state_machine] added

---
+++ state_machine.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+import array
+
+__all__ = ('StateMachine',)
+
+
+class StateMachine(object):
+    """Graph state machine
+    """
+    __slots__ = ('graph', 'state', 'names',)
+
+    def __init__(self, graph, names=None):
+        self.graph = graph
+        self.names = names
+        self.state = 0
+
+    def __call__(self, state):
+        if self.graph[self.state] & (1 << state):
+            if self.state == state:
+                return False
+            self.state = state
+            return True
+        else:
+            raise ValueError('invalid state transition {} -> {}'.
+                             format(self.state_name(), self.state_name(state)))
+
+    def state_name(self, state=None):
+        state = self.state if state is None else state
+        if self.names:
+            return self.names[state]
+        else:
+            return str(state)
+
+    @classmethod
+    def compile_graph(cls, graph_tree):
+        """Create transition graph from dictionary representation
+
+        Dictionary with state as key and available transition states as its
+        value. State must be and integer. Initial state is 0.
+        """
+        max_state = 0
+        graph = {}
+        for src, dsts in graph_tree.items():
+            max_state = max(max_state, src)
+            for dst in dsts:
+                max_state = max(max_state, dst)
+                graph[src] = graph.get(src, 0) | 1 << dst
+
+        if max_state >= 63:
+            return list(graph.get(src, 0) for src in range(max_state + 1))
+        else:
+            return array.array('B' if max_state <= 7 else
+                               'H' if max_state <= 15 else
+                               'I' if max_state <= 31 else 'Q',
+                               (graph.get(src, 0) for src in range(max_state + 1)))
+
+
+# vim: nu ft=python columns=120 :
