ae3929d2f4a226f8d73b94a1db38edd7fec6c6de
aslpavel <asl.pavel@gmail.com>
2013-04-12 20:02:48 +0400

    [app] added: minimalistic application decorator

---
+++ app.py
@@ -0,0 +1,22 @@
+import functools
+from .core import Core
+from .monad import do_return, async, async_any, async_all, async_block
+
+__all__ = ('app', 'do_return', 'async', 'async_any', 'async_all', 'async_block',)
+
+
+def app(main):
+    """Application decorator
+
+    Create asynchronous application with provide main function.
+    """
+    @functools.wraps(main)
+    def app_main(*a, **kw):
+        with Core.local() as core:
+            app_future = async(main)(*a, **kw).future()
+            app_future(lambda _: core.dispose())
+            if not core.disposed:
+                core()
+        assert app_future.completed
+        app_future.result
+    return app_main
