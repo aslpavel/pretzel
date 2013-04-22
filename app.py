2da421f1432526884df197f96e2938edba1c9e0d
aslpavel <asl.pavel@gmail.com>
2013-04-22 12:54:02 +0400

    [app] added: common trampoline app_run

--- app.py
+++ app.py
@@ -1,42 +1,39 @@
 """Asynchronous application trampoline decorator
 """
-import functools
+from functools import wraps
 from .core import Core
 from .monad import (async, async_green, async_any, async_all, async_block,
                     do_return, bind_green,)
 
-__all__ = ('app', 'app_green', 'async', 'async_green', 'async_any', 'async_all',
-           'async_block', 'do_return', 'bind_green',)
+__all__ = ('app', 'app_green', 'app_run', 'async', 'async_green', 'async_any',
+           'async_all', 'async_block', 'do_return', 'bind_green',)
 
 
 def app(main):
     """Application decorator
 
-    Create main coroutine and initialize asynchronous context.
+    Create application from generator function.
     """
-    return app_with_opts(main, async)
+    return wraps(main)(lambda *a, **kw: app_run(async(main)(*a, **kw)))
 
 
 def app_green(main):
     """Application decorator
 
-    Create main coroutine and initialize asynchronous context.
+    Create application from greenlet main function.
     """
-    return app_with_opts(main, async_green)
+    return wraps(main)(lambda *a, **kw: app_run(async_green(main)(*a, **kw)))
 
 
-def app_with_opts(main, async, poller=None):
-    """Application decorator
+def app_run(cont):
+    """Application trampoline
 
-    Create main coroutine and initialize asynchronous context.
+    Run continuation as application.
     """
-    @functools.wraps(main)
-    def app_main(*a, **kw):
-        with Core.local(Core(poller)) as core:
-            app_future = async(main)(*a, **kw).future()
-            app_future(lambda _: core.dispose())
-            if not core.disposed:
-                core()
-        assert app_future.completed
-        return app_future.result.value
-    return app_main
+    with Core.local(Core()) as core:
+        app_future = cont.__monad__().future()
+        app_future(lambda _: core.dispose())
+        if not core.disposed:
+            core()
+    assert app_future.completed
+    return app_future.result.value
