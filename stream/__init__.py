75d0ff0ddb6d2375afecac348b8fa6325e4332d3
Pavel Aslanov <asl.pavel@gmail.com>
2013-05-21 16:20:25 +0400

    [stream] fixed: reference on removed pipe module from __init__

--- stream/__init__.py
+++ stream/__init__.py
@@ -1,4 +1,4 @@
-from . import stream, file, sock, sock_ssl, wrapped, buffered, pipe
+from . import stream, file, sock, sock_ssl, wrapped, buffered
 
 from .stream import *
 from .file import *
@@ -6,10 +6,9 @@ from .sock import *
 from .sock_ssl import *
 from .wrapped import *
 from .buffered import *
-from .pipe import *
 
 __all__ = (stream.__all__ + file.__all__ + sock.__all__ + sock_ssl.__all__ +
-           wrapped.__all__ + buffered.__all__ + pipe.__all__)
+           wrapped.__all__ + buffered.__all__)
 
 
 def load_tests(loader, tests, pattern):
