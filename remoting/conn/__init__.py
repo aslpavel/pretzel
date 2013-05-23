914f2af054a0be1f5d60d4b0ea8c3c3f0c1db97a
Pavel Aslanov <asl.pavel@gmail.com>
2013-05-23 19:08:09 +0400

    [remoting] moved: composed.py -> composite.py

--- remoting/conn/__init__.py
+++ remoting/conn/__init__.py
@@ -2,6 +2,6 @@ from . import fork, shell, ssh
 from .fork import *
 from .shell import *
 from .ssh import *
-from .composed import *
+from .composite import *
 
-__all__ = fork.__all__ + shell.__all__ + ssh.__all__ + composed.__all__
+__all__ = fork.__all__ + shell.__all__ + ssh.__all__ + composite.__all__
