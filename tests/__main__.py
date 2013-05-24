5bd79c1a31a2d1e9a5c67f55ab6c44023fa149be
Pavel Aslanov <asl.pavel@gmail.com>
2013-05-24 11:13:45 +0400

    [tests] changed: submodule is now runnable

---
+++ tests/__main__.py
@@ -0,0 +1,28 @@
+import os
+import unittest
+import argparse
+from .. import __package__ as pretzel
+
+def main():
+    """Run unit tests on pretzel pacakge
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--coverage', dest='coverage',
+                        action='store_true', help='run coverage')
+    opts = parser.parse_args()
+
+    if opts.coverage and os.environ.get('TRAVIS_PYTHON_VERSION') != 'pypy':
+        import coverage
+        cov = coverage.coverage()
+        cov.start()
+        try:
+            unittest.main(module=pretzel)
+        finally:
+            cov.stop()
+            cov.save()
+            cov.report(show_missing=False)
+    else:
+        unittest.main(module=pretzel)
+
+if __name__ == '__main__':
+    main()
