9807a3d6ed9608a8f2122c52964827f54d2c045e
Pavel Aslanov <asl.pavel@gmail.com>
2013-05-27 15:49:47 +0400

    [process] unittest: custom standard streams

--- tests/process.py
+++ tests/process.py
@@ -1,9 +1,10 @@
 import textwrap
 import unittest
-
-from ..process import Process, ProcessError, PIPE, DEVNULL, process_call
-from ..monad import Cont
+import tempfile
 from . import async_test
+from ..monad import Cont
+from ..dispose import CompDisp
+from ..process import Process, ProcessError, PIPE, DEVNULL, process_call
 
 __all__ = ('ProcessTest',)
 
@@ -11,26 +12,35 @@ __all__ = ('ProcessTest',)
 class ProcessTest(unittest.TestCase):
     @async_test
     def test_call(self):
-        command = ['python', '-c', textwrap.dedent("""
-            import sys
-            for value in range(int(input())):
-                if value % 2 == 1:
-                    sys.stderr.write(str(value))
-                else:
-                    sys.stdout.write(str(value))
-            sys.stderr.flush()
-            sys.stdout.flush()
-            sys.exit (117)
-            """)]
-
         out, err, code = yield process_call(command, input=b'10', check=False)
         self.assertEqual(code, 117)
         self.assertEqual(out, b'02468')
         self.assertEqual(err, b'13579')
 
     @async_test
+    def test_call_fd(self):
+        with CompDisp() as dispose:
+            stdin = dispose.add(tempfile.TemporaryFile())
+            stdout = dispose.add(tempfile.TemporaryFile())
+            stderr = dispose.add(tempfile.TemporaryFile())
+
+            stdin.write(b'10')
+            stdin.seek(0)
+            out, err, code = yield process_call(command, stdin=stdin, stdout=stdout,
+                                                stderr=stderr, check=False)
+
+            self.assertEqual(code, 117)
+            self.assertEqual(out, None)
+            self.assertEqual(err, None)
+            stdout.seek(0)
+            self.assertEqual(stdout.read(), b'02468')
+            stderr.seek(0)
+            self.assertEqual(stderr.read(), b'13579')
+
+    @async_test
     def test_cleanup(self):
-        with (yield Process(['cat'], stdin=PIPE, stdout=PIPE, stderr=PIPE, check=False)) as proc:
+        with (yield Process(['cat'], stdin=PIPE, stdout=PIPE, stderr=PIPE,
+                            check=False)) as proc:
             self.assertTrue(proc.stdin.close_on_exec())
             self.assertTrue(proc.stdout.close_on_exec())
             self.assertTrue(proc.stderr.close_on_exec())
@@ -54,8 +64,21 @@ class ProcessTest(unittest.TestCase):
                          (b'', b'', 0))
 
     @async_test
-    def test_check(self):
+    def test_check_option(self):
         out, err, code = yield process_call(['false'], check=False)
         self.assertNotEqual(code, 0)
         with self.assertRaises(ProcessError):
             yield process_call(['false'], check=True)
+
+
+command = ['python', '-c', textwrap.dedent("""
+    import sys
+    for value in range(int(input())):
+        if value % 2 == 1:
+            sys.stderr.write(str(value))
+        else:
+            sys.stdout.write(str(value))
+    sys.stderr.flush()
+    sys.stdout.flush()
+    sys.exit (117)
+    """)]
