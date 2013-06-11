98854942e6e72e3ef5bacf7715acd83013dfa2a3
Pavel Aslanov <asl.pavel@gmail.com>
2013-06-11 18:08:37 +0400

    [process] changed: better error message in checked process

--- process.py
+++ process.py
@@ -163,7 +163,8 @@ class Process(object):
                 def check_status():
                     status = yield waitpid
                     if self.opts.check and status != 0:
-                        raise ProcessError('non-zero exit status: {}'.format(status))
+                        raise ProcessError('command \'{}\' returned non-zero exit '
+                                           'status {}'.format(self.opts.command, status))
                     do_return(status)
                 check_status()(self.opts.status)
 
