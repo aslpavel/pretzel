b3baf28a8dfcf74610bb9cba6ad54e7d1e122702
Pavel Aslanov <asl.pavel@gmail.com>
2013-07-10 11:41:41 +0400

    [remoting.conn.ssh] added: ssh agent forwarding

--- remoting/conn/ssh.py
+++ remoting/conn/ssh.py
@@ -10,21 +10,22 @@ __all__ = ('SSHConnection',)
 class SSHConnection(ShellConnection):
     """SSH Connection
     """
-    def __init__(self, host, port=None, identity_file=None, ssh_exec=None,
+    def __init__(self, host, port=None, ssh_identity=None, ssh_exec=None,
                  py_exec=None, environ=None, bufsize=None, hub=None, core=None):
         self.host = host
         self.port = port
-        self.identity_file = identity_file
+        self.ssh_identity = ssh_identity
         self.ssh_exec = ssh_exec or 'ssh'
 
         # ssh command
         command = [
             self.ssh_exec,          # command
+            '-A',                   # forward ssh agent
             '-T',                   # disable pseudo-tty allocation
             '-o', 'BatchMode=yes',  # never ask password
             self.host,              # host
         ]
-        command.extend(('-i', self.identity_file) if self.identity_file else [])
+        command.extend(('-i', self.ssh_identity) if self.ssh_identity else [])
         command.extend(('-p', self.port) if self.port else [])
 
         ShellConnection.__init__(self, command=command, escape=True,
